b35974382cd4a3a19a54fb20fa21934f643c8fa1
surajith <surajithreddy@gmail.com>
2018-11-24 21:37:55 +0530

    Adding Classes for Parks and Streets

---
+++ starter/ES6CodeChallenge.js
@@ -0,0 +1,18 @@
+class Parks{
+    constructor(name,buildYear){
+        this.name=name;
+        this.buildYear=buildYear;
+    }  
+class Streets{
+    constructor(name,buildYear){
+        this.name=name;
+        this.buildYear=buildYear;
+    }
+}
+
+
+
+
+
+
+
