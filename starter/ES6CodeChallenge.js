a3578aad4a683e81dc39aa87de06ffd3ae446d9f
surajith <surajithreddy@gmail.com>
2018-11-24 21:48:26 +0530

    Adding Park subclass from Parks class

--- starter/ES6CodeChallenge.js
+++ starter/ES6CodeChallenge.js
@@ -3,12 +3,22 @@ class Parks{
         this.name=name;
         this.buildYear=buildYear;
     }  
+}
 class Streets{
     constructor(name,buildYear){
         this.name=name;
         this.buildYear=buildYear;
     }
 }
+
+class Park extends Parks{
+    constructor(name,buildYear,parkArea,numTrees){
+        super(name,buildYear)
+        this.parkArea=parkArea;
+        this.numTrees=numTrees;
+    }
+}
+
 
 
 
